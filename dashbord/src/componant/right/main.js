665d9c45939911cb2f60467abec3ae33ea3f4d1c
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-04 15:14:03 +0100

    new

--- dashbord/src/componant/right/main.js
+++ dashbord/src/componant/right/main.js
@@ -8,7 +8,7 @@ const doneData = data.filter(item => item.tag == "done")
 const Links = ()=>
 {
     return (
-        <div className=" main flex px-2  border  bg-gray-200 py-3 h-auto" >
+        <div className=" main flex px-2  border  bg-gray-200 py-3 h-auto c" >
 
 
             {/**these are to do data */}
